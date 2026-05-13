1c061a153755e84bd06436a4fe3b123e222e9280
Mendel <MendelCohen770@gmail.com>
2026-05-13 15:47:51 +0300

    add function signature

---
+++ Hangman.py
@@ -0,0 +1,40 @@
+
+MAX_TRIES = 8
+lst_of_words = []
+letters_guessed_by_the_user = []
+
+
+def get_random_word():
+    pass
+
+
+def hide_the_word():
+    pass
+
+
+def convert_word_to_lst():
+    pass
+
+
+def init_the_game():
+    pass
+
+
+def get_user_guess():
+    pass
+
+
+def validate_guess():
+    pass
+
+
+def print_the_word():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
